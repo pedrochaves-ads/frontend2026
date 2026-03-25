d57b59dede0f142704581aadd28360cb49475a92
Pedro Chaves <pedrochavesa@hotmail.com>
2026-03-25 00:03:45 +0000

    adicionando pasta da aula

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+const botao = document.querySelector("button");
+
+botao.addEventListener('click'), (e) => {
+    const matriculaInput = document.querySelector("#matricula");
+    const senhaInput = document.querySelector("#senha");
+
+    if (matriculaInput.value == '') {
+        console.log("Matrícula é obrigatória")
+    }
+    )
+}
